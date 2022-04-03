9e8d3e8e565c77e001c2643cf6bb7a14bfd2aa4a
2nchanter <2nchanter@gmail.com>
2022-04-03 14:08:22 +0900

    C

---
+++ 컴퓨터언어_김종국/C5_조건문.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+# 영상, 영하
+int main(void) {
+int temp = 0;
+
+printf("insert temp: ");
+scanf("%d", &temp);
+
+if (temp < 0){
+  printf("영하\n");
+}
+else if (temp >= 0){
+  printf("영상\n");
+}
+else{
+  printf("N/A\n");
+}
+printf("%d \n", temp);
+}
+
+# 홀짝
+int main(void) {
+int x;
+
+printf("정수를 입력하시오:\n");
+scanf("%d", &x);
+
+if (x%2==1)
+  printf("입력된 정수는 홀수입니다.");
+else
+  printf("입력된 정수는 짝수입니다.");
+}
